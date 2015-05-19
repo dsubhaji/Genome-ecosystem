2b344d503cb3216f5d0a514020df0cf93855b6d7
Siti Nurafifah <1301079H@student.tp.edu.sg>
2015-05-19 09:27:31 +0800

    without choice 0 yet

---
+++ people-pre-analysis.R
@@ -0,0 +1,85 @@
+library(ggplot2)
+gnome = dbConnect(MySQL(), user='root', password='', dbname= 'gnome', host='localhost')
+
+
+repeat{
+option <- readline(prompt="Please select from following options: 
+         \n0: Generate csv file
+         \n1: Number of committers
+         \n2: Number of authors
+         \n3: Number of repo each person commits to
+         \n4: Number of repo: authored commits to
+         \n5: Exit ")
+
+
+print(option)
+
+if(option == 0) {
+  
+  
+}
+if(option== 1) {
+  #fetch the number of times each person is the committer
+  pCommitters = fetch(dbSendQuery(gnome, "SELECT people.id as 'people',count(scmlog.id) as 'committers',  people.email FROM scmlog
+                                          JOIN people ON scmlog.committer_id = people.id
+                                          GROUP BY scmlog.committer_id;"))
+  
+  #plot histogram for Number of times each person is the committer
+  pCommitsHist <- ggplot(pCommitters, aes(committers)) + geom_histogram() + ylab("Frequency") +ggtitle("No. Of people as committers")
+  ggsave("R language/1st Project/graphs/people-pre-analysis/Committer/pCommitterHistogram.png")
+  
+ 
+  #plot boxplot for number of times each person is the committer
+  #boxplot with continuous x
+  pCommitsBox <- ggplot(pCommitters, aes(people,committers)) + geom_boxplot() + ylab("Number of people")+ ggtitle("No. Of people as committers")
+  ggsave("R language/1st Project/graphs/people-pre-analysis/Committer/pCommitterBoxPlot.png")
+}
+if(option==2){
+  #fetch the number of times each person is the author
+  pAuthors = fetch(dbSendQuery(gnome,"SELECT  people.id as 'people',count(scmlog.id) as 'authors' , people.email FROM scmlog
+                                      JOIN people ON scmlog.author_id = people.id
+                                      GROUP BY scmlog.author_id;"))
+  
+  #plot histogram for the number of times each person is the author
+  pAuthorsHist <- ggplot(pAuthors, aes(authors)) + geom_histogram() + ylab("Frequency") +ggtitle("No. Of people as authors")
+  ggsave("R language/1st Project/graphs/people-pre-analysis/Author/pAuthorsHistogram.png")
+
+  #plot boxplot for the number of times each person is the author
+  pAuthorsBox <- ggplot(pAuthors, aes(people, authors)) + geom_boxplot() + ylab("Number of authors") +ggtitle("No. Of people as authors")
+
+}
+if(option==3) {
+  #fetch number of different repositories each person has committed to
+pRepoOfCommitters = fetch(dbSendQuery(gnome, "select committer_id as 'committer',count(distinct repository_id) as 'repository'
+                                              from scmlog
+                                              group by committer_id;"))
+#plot histogram for the  number of different repositories each person has committed to
+pRepoCommitHist <- ggplot(pRepoOfCommitters, aes(repository)) + geom_histogram() + ylab("Frequency") +ggtitle("No. Of different repos per committer")
+ggsave("R language/1st Project/graphs/people-pre-analysis/RepoOfCommitters/pRepoCommitterHist.png")
+
+#plot boxplot for the number of different repositories each person has committed to
+pRepoCommitBox <- ggplot(pRepoOfCommitters, aes(committer, repository)) + geom_boxplot() + ylab("Number of repository") +ggtitle("No. Of different repos per committer")
+ggsave("R language/1st Project/graphs/people-pre-analysis/RepoOfCommitters/pRepoCommitterBoxPlot.png")
+
+
+}
+if(option==4) {
+  
+ #fetch number of different repositories each person has authored commits to
+  pRepoOfAuthors = fetch(dbSendQuery(gnome, "select author_id as 'author' ,count(distinct repository_id) as 'repository'
+                                              from scmlog
+                                              group by author_id;"))
+  
+  #plot histogram for the number of different repositories each person has authored commits to
+  pRepoAuthorHist <- ggplot(pRepoOfAuthors, aes(repository)) + geom_histogram() + ylab("Frequency") +ggtitle("No. Of different repos per authors")
+  ggsave("R language/1st Project/graphs/people-pre-analysis/RepoOfAuthors/pRepoAuthorHist.png")
+
+  #plot boxplot for the number of different repositories each person has authored commits to
+  pRepoAuthorBox <- ggplot(pRepoOfAuthors, aes(author, repository)) + geom_boxplot() + ylab("Number of repository") +ggtitle("No. Of different repos per author")
+  ggsave("R language/1st Project/graphs/people-pre-analysis/RepoOfAuthors/pRepoAuthorBoxPlot.png")
+  
+}
+if(option==5) {
+  break; }
+
+}
